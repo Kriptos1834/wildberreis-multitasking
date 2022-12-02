09ce06be59651993b88749bfb0617d60c340f803
Mkholkin Debian <mkholkin@224468.fornex.cloud>
2022-12-03 00:21:37 +0300

    commit to pull

--- wildberries_backend/wildberries_backend/settings.py
+++ wildberries_backend/wildberries_backend/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for wildberries_backend project.
 
@@ -122,8 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # Daphne
 ASGI_APPLICATION = 'wildberries_backend.asgi.application'
 CHANNEL_LAYERS = {
@@ -136,7 +135,7 @@ CHANNEL_LAYERS = {
 }
 
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 STATICFILES_DIRS.append(os.path.join(BASE_DIR, '../wildberries_react/wildberries/build/static'))
